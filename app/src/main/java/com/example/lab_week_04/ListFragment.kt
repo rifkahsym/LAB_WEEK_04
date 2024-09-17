cb8faf875570328a9c9c5aa402821361e6cd6a3a
rifkahsym <rifkahsyamsaibah@gmail.com>
2024-09-17 17:17:08 +0700

    update: activity_main.xml colors.xml DetailFragment.kt ListFragment.kt fragment_detail.xml fragment_list.xml nav_graph.xml strings.xml themes.xml

--- app/src/main/java/com/example/lab_week_04/ListFragment.kt
+++ app/src/main/java/com/example/lab_week_04/ListFragment.kt
@@ -1,12 +1,12 @@
 package com.example.lab_week_04
 
-import android.content.Context
 import android.os.Bundle
-import android.view.LayoutInflater
 import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import com.example.lab_week_04.R.*
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -18,17 +18,10 @@ private const val ARG_PARAM2 = "param2"
  * Use the [ListFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
+class ListFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
@@ -36,7 +29,7 @@ class ListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        return inflater.inflate(layout.fragment_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -44,32 +37,18 @@ class ListFragment : Fragment() {
         val coffeeList = listOf<View>(
             view.findViewById(R.id.affogato),
             view.findViewById(R.id.americano),
-            view.findViewById(R.id.latte),
-            view.findViewById(R.id.cappuccino),
-            view.findViewById(R.id.mocha),
-            view.findViewById(R.id.espresso)
+            view.findViewById(R.id.latte)
         )
 
-//      coffeeList.forEach{
-//            it.setOnClickListener(this)
-//        }
-
-        coffeeList.forEach { coffee ->
+        coffeeList.forEach{ coffee ->
             val fragmentBundle = Bundle()
             fragmentBundle.putInt(COFFEE_ID, coffee.id)
             coffee.setOnClickListener(
                 Navigation.createNavigateOnClickListener(
-                    R.id.coffee_id_action, fragmentBundle
-                )
+                    R.id.action_ListFragment_to_DetailFragment, fragmentBundle)
             )
         }
     }
-
-//    override fun onClick(v: View?) {
-//        v?.let{
-//                coffee -> coffeeListener.onSelected(coffee.id)
-//        }
-//    }
 
     companion object {
         const val COFFEE_ID = "COFFEE_ID"
